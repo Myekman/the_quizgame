9a9d78df39e1dc5a45c6c6c37e918fc50aa55f8f
Myekman <my@traningstemplet.se>
2023-04-14 15:43:24 +0000

    Add questions to a dictionary

--- run.py
+++ run.py
@@ -1,3 +1,41 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+score = 0
+wrong_answer = 0
+
+print("Welcome to this game!")
+
+name = input("\nEnter your name: ")
+print(f"\nHello {name}")
+
+play = input("\nDo you like to play, yes or no?")
+
+if play == "yes":
+    print("\nLet's do this!")
+    print("________________________")
+elif play == "no":
+    print("Goodbye..")
+else:
+    print("invalid choice, try again!") 
+
+questions = [
+  {
+    'question': "\nA square has ___ sides?",
+    'answer': '4'
+  },
+    {
+    'question': "\nA triangle has ___ sides?",
+    'answer': '3'
+  },
+    {
+    'question': "\nA circle has ___ sides?",
+    'answer': '0'
+  }
+]
+
+for question in questions:
+    show_question = question.get("question")
+    print(show_question)
+
+    user_answer = input("Enter answer: ")
+
+    correct_answer = question.get("answer")
