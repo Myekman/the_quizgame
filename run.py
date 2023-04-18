94767241f796430a0443e9576f23391876a53e63
Myekman <my@traningstemplet.se>
2023-04-18 18:59:23 +0000

    fix a few indentention problems

--- run.py
+++ run.py
@@ -6,18 +6,18 @@ name = input("\nEnter your name: ")
 print(f"\nHello {name}!")
 
 while True: 
-  play = input("\nDo you like to play? please type yes or no: ")
+    play = input("\nDo you like to play? please type yes or no: ")
 
-  if play == "yes":
-    print("\nLet's do this!")
-    print("________________________")
-    print("\nLEVEL 1!")
-    break
-  elif play == "no":
-    print("Goodbye..")
-    exit()
-  else:
-    print("\nInvalid choice, try again!") 
+    if play == "yes":
+        print("\nLet's do this!")
+        print("________________________")
+        print("\nLEVEL 1!")
+        break
+    elif play == "no":
+        print("Goodbye..")
+        exit()
+    else:
+        print("\nInvalid choice, try again!") 
 
 
 #level 1 in game
@@ -37,40 +37,40 @@ questions = [
 ]
 
 def level_one(questions):
-  score = 0
-  wrong_answer = 0
-  you_win = False
+    score = 0
+    wrong_answer = 0
+    you_win = False
   
-  for question in questions:
-    show_question = question.get("question")
-    print(show_question)
+    for question in questions:
+        show_question = question.get("question")
+        print(show_question)
 
-    user_answer = input("Enter answer: ")
+        user_answer = input("Enter answer: ")
   
-    correct_answer = question.get("answer")
+        correct_answer = question.get("answer")
 
-    if user_answer == correct_answer:
-      score = score + 1
-      print("\nCorrect!")
-    elif user_answer != correct_answer:
-      wrong_answer = wrong_answer + 1
-      print(f"\nIncorrect.. The right answer was: {correct_answer}") 
+        if user_answer == correct_answer:
+            score = score + 1
+            print("\nCorrect!")
+        elif user_answer != correct_answer:
+            wrong_answer = wrong_answer + 1
+            print(f"\nIncorrect.. The right answer was: {correct_answer}") 
 
-    if score == 3:
-      print("Good job, you got the highest score!")
-      print("\n\nWelcome to level 2!")
-      print("________________________")
-      you_win = True
-    elif score < 3:
-      print(f"\nYour score = {score}")
-      print(f"\nWrong answer: {wrong_answer}")
+        if score == 3:
+            print("Good job, you got the highest score!")
+            print("\n\nWelcome to level 2!")
+            print("________________________")
+            you_win = True
+        elif score < 3:
+            print(f"\nYour score = {score}")
+            print(f"\nWrong answer: {wrong_answer}")
 
-  while you_win == False:
-    print("\nyou need ... to move to next level")
-    print("\ntry again")
-    level_one(questions)
-    if you_win:
-      break
+    while you_win == False:
+        print("\nyou need ... to move to next level")
+        print("\ntry again")
+        level_one(questions)
+        if you_win:
+            break
 
 
 #level 2
@@ -83,23 +83,23 @@ def run_test_2():
     print("\nLEVEL 2!")
 
     while guess != secret_number and not(out_of_guesses):
-      if guess_count < guess_limit:
-        print("\nCan you guess the right number between 0-15? ")
-        guess = int(input("enter guess: "))
-        guess_count += 1
-      else:
-        out_of_guesses = True
+        if guess_count < guess_limit:
+            print("\nCan you guess the right number between 0-15? ")
+            guess = int(input("enter guess: "))
+            guess_count += 1
+        else:
+            out_of_guesses = True
 
     if out_of_guesses:
-      print("\nYOU LOSE!")
-      print("\nTry again....")
-      print("________________________")
-      run_test_2()
+        print("\nYOU LOSE!")
+        print("\nTry again....")
+        print("________________________")
+        run_test_2()
     else:
-      print("\nCorrect!")
-      print("Welcome to level 3!")
-      print("________________________")
-      print()
+        print("\nCorrect!")
+        print("Welcome to level 3!")
+        print("________________________")
+        print()
   
 
 # #level 3 in game
@@ -116,16 +116,17 @@ level3 = [
   Question(question_prompts[2], "b"),
 ]
 
+
 def run_test_3(level3):
-  print("LEVEL 3!")
-  print()
+    print("LEVEL 3!")
+    print()
   
-  score = 0
-  for question in level3:
-    answer = input(question.prompt)
-    if answer == question.answer:
-      score = score + 1
-  print("You got " + str(score) + "/" + str(len(level3)) + " Correct")
+    score = 0
+    for question in level3:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score = score + 1
+    print("You got " + str(score) + "/" + str(len(level3)) + " Correct")
 
 
 level_one(questions)
