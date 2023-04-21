71f6f5c96f40c44a592107f3d6ad94af8837a625
Myekman <my@traningstemplet.se>
2023-04-21 09:35:24 +0000

    import and insall gspread to connecting to Api

--- run.py
+++ run.py
@@ -1,5 +1,24 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
 import os
 from Question import Question
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('the_quizgame')
+
+result = SHEET.worksheet('result')
+
+data = result.get_all_values()
+print(data)
 
 print("Welcome to this game!")
 
